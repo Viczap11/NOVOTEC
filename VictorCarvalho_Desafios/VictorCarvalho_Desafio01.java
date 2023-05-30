053134127c2eebb54f2872dc78da541382b12f8d
Viczap11 <131813838+Viczap11@users.noreply.github.com>
2023-05-30 18:39:56 -0300

    Add files via upload

--- VictorCarvalho_Desafios/VictorCarvalho_Desafio01.java
+++ VictorCarvalho_Desafios/VictorCarvalho_Desafio01.java
@@ -1,30 +1,47 @@
 /*
-Conta até 200 e mostra os múltiplos de dez
+Recebe números até  ser mencionado o número 0 e mostra qual o maior
 Autor:Victor Geovanne
 30/05/2023
 */
+
+//Importa a classe Scanner
+import java.util.Scanner;
 public class VictorCarvalho_Desafio01 {
     public static void main(String [] args){
-    
-    //Declaração das variáveis
-    int cont1, cont2;
 
-    //Inicialização das variáveis
-     cont1 = cont2 = 0 ;
+        //Declaração de variáveis
+        Scanner leitor = new Scanner(System.in);
+        int n,maior ;
 
-     //Estrutura de repetição
-     while(cont1<200){
-        //Saídas 
-        System.out.println(cont1);
-        cont1++;
-        cont2++;
-        //Estrutura condicional para cada múltiplo de 10
-        if( cont2 == 10 ){
-            System.out.println("O número " + cont1 + " é múltiplo de 10");
-            cont2 = 0;
-        }
+        //Inicialização de variáveis
+        n=maior=0;
 
+        //Instrução pro usuário e recepção de variáveis
+        System.out.println("Digite um número ");
+        n=leitor.nextInt();
+
+        //Estrutura condicional para se o número não for 0
+        if(n!=0){
+            
+            //Estrutura de repetição enquanto o número não for 0
+            while(n!=0){
+                
+                 //Instrução pro usuário e recepção de variáveis
+                System.out.println("Digite um número ");
+                n = leitor.nextInt();   
+                    
+                //Estrutura condicional se o número for maior que o maior número anterior 
+                if(n>maior){
+                            maior=n;
+                        }
+                    }
+            }
+       
+            //Saída
+            System.out.println(maior + " foi o maior número");
+        
+            //Encerramento do leitor
+            leitor.close();
     }
-     }   
-    }    
-
+    
+}
