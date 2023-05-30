250742d1b505176188fb412642d5de2f99391964
Viczap11 <131813838+Viczap11@users.noreply.github.com>
2023-05-30 17:34:25 -0300

    Inserção da lista 3 e desafios dela

---
+++ VictorCarvalho_Exercício01.java
@@ -0,0 +1,21 @@
+/*
+Mostra os números de 0 a 1000
+Autor:Victor Geovanne
+30/05/2023
+*/
+
+public class VictorCarvalho_Exercício01{
+    public   static void main(String [] args){
+        
+        //Declaração e inicialização das variáveis
+        int numero = 1;
+
+        //Estrutura de repetição e saídas
+        while(numero<1000){
+            System.out.println(numero);
+            numero++;
+
+        }
+
+    }
+}
