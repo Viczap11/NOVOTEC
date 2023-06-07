054560a22c68dbc891db3f05ecc9ee4e4fc5d925
Viczap11 <131813838+Viczap11@users.noreply.github.com>
2023-06-07 17:23:33 -0300

    Nova lista

---
+++ VictorCarvalho_Exercício01.java
@@ -0,0 +1,26 @@
+/*
+Classe alimento e 10 atributos
+Victor Geovanne
+07/06/2023
+*/
+
+public class VictorCarvalho_Exercício01 {
+    public static void main(String [] args){
+
+    }
+    
+}
+class Alimentos{
+
+/* CRIAÇÃO DOS ATRIBUTOS */
+
+String textura, gosto, corDaCasca, corDeDentro, tamanho, cheiro  ;
+float peso, precoDoAlimento;
+boolean comestivel;  
+int quantidadeDoAlimento;
+
+ 
+
+
+
+}
