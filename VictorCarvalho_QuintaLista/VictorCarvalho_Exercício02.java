c322f0688727a0e14558176f5a2ef632b61f6b9c
Viczap11 <131813838+Viczap11@users.noreply.github.com>
2023-06-07 17:51:52 -0300

    Add files via upload

---
+++ VictorCarvalho_QuintaLista/VictorCarvalho_Exercício02.java
@@ -0,0 +1,32 @@
+/*
+Classe chamada Cinto e 5 métodos
+Victor Geovanne
+07/06/2023
+*/
+
+public class VictorCarvalho_Exercício02 {
+    public static void main (String [] args){
+ 
+    }
+}
+class Cinto{
+
+/*CRIAÇÃO DOS MÉTODOS*/
+void Bater(){
+    System.out.println("Você vai apanhar");
+}
+void Prender(){
+    System.out.println("Seguro sua calça");
+}
+void Soltarse(){
+    System.out.println("Posso cair");
+}
+void Cair(){
+    System.out.println("Posso cair");
+}
+void Machucar(){
+    return ;
+}
+}
+    
+
